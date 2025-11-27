58f11f43084969c0d1151142fef83c476d14f242
Sohas Sultana <sohasultana34@gmail.com>
2025-11-26 17:10:55 -0800

    changed text size done

--- js/sketches/viz/sketch_tariff.js
+++ js/sketches/viz/sketch_tariff.js
@@ -54,7 +54,7 @@
 
       // ---- TITLE ----
       p.textAlign(p.CENTER, p.CENTER);
-      p.textSize(22);
+      p.textSize(15);
       p.fill(0);
       p.text("Imports and Exports of Different Countries 2023–2025", 450, 45);
 
