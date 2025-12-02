137b595a3a2173f0933e1e200986632a5952bebb
Gunner Dohrenwend <139017216+gunnerdoh@users.noreply.github.com>
2025-12-01 17:09:44 -0800

    working on fixing stacked bar

--- js/sketches/GDPStackedBar.js
+++ js/sketches/GDPStackedBar.js
@@ -3,9 +3,11 @@
     // Variables
     var importsData;
     var exportsData;
-    var top10Data = [];
+    var top8data = [];
     var sectors = [];
     var colors = [];
+    var importColors = ['#ffcc99', '#994d00', '#ffb366', '#cc6600', '#ff8000', '#663300', '#ff9933', '#aa5500'];
+    var exportColors = ['#96b2f5', '#0c2e7c', '#2d65ea', '#071b4a', '#618cef', '#0a2563', '#144ac9', '#0f3795'];
     var showing = "imports";
     var selectedYear = 2018;
     var yearSelect;
@@ -53,25 +55,11 @@
       }
     
       sectors = sectorSet;
-    
-      // Generate colors for sectors
-      colors = [];
-      for (var i = 0; i < sectors.length; i++) {
-        var r, g, b;
       
-        if (showing === "imports") {
-          r = (255 / 2) + (10 * i);
-          g = (i * (255 / 11));
-          b = 255;
-        } else {
-          r = 255;
-          g = (i * (255 / 11));
-          b = (i * (255 / 11));
-        }
-        colors.push(p.color(r, g, b)); 
-      }
+      // Set colors based on showing mode
+      colors = showing === "imports" ? importColors : exportColors;
     
-      // Compute total per country for top 10
+      // Compute total per country for top 8
       var totalsArr = [];
       for (var country in totals) {
         var data = totals[country];
@@ -84,13 +72,13 @@
     
       totalsArr.sort(function(a, b) { return b.total - a.total; });
       
-      top10Data = [];
-      for (var i = 0; i < Math.min(10, totalsArr.length); i++) {
+      top8data = [];
+      for (var i = 0; i < Math.min(8, totalsArr.length); i++) {
         var entry = { country: totalsArr[i].country };
         for (var sector in totalsArr[i].data) {
           entry[sector] = totalsArr[i].data[sector];
         }
-        top10Data.push(entry);
+        top8data.push(entry);
       }
     };
     
@@ -128,35 +116,38 @@
       p.textSize(18);
       p.textAlign(p.CENTER);
       var title = showing.charAt(0).toUpperCase() + showing.slice(1);
-      p.text("Top 10 Countries by " + title + " (" + selectedYear + ")", p.width / 2, 50);
+      p.text("Top 8 Countries by " + title + " (" + selectedYear + ")", p.width / 2, 50);
     
-      if (top10Data.length === 0) return;
+      if (top8data.length === 0) return;
     
       var margin = 120;
       var chartWidth = p.width - 200 - 2 * margin;
       var chartHeight = p.height - 2 * margin;
-      var barWidth = chartWidth / top10Data.length * 0.6;
+      var barWidth = chartWidth / top8data.length * 0.6;
     
       // Find maximum total value for scaling
       var maxVal = 0;
-      for (var i = 0; i < top10Data.length; i++) {
+      for (var i = 0; i < top8data.length; i++) {
         var total = 0;
         for (var j = 0; j < sectors.length; j++) {
-          total += top10Data[i][sectors[j]] || 0;
+          total += top8data[i][sectors[j]] || 0;
         }
         if (total > maxVal) maxVal = total;
       }
     
       p.textSize(12);
-      for (var i = 0; i < top10Data.length; i++) {
-        var d = top10Data[i];
-        var x = margin + i * (chartWidth / top10Data.length) + (chartWidth / top10Data.length - barWidth) / 2;
+        
+      for (var i = 0; i < top8data.length; i++) {
+        var d = top8data[i];
+        var x = margin + i * (chartWidth / top8data.length) + (chartWidth / top8data.length - barWidth) / 2;
     
         var yBottom = p.height - margin;
-        for (var j = 0; j < sectors.length; j++) {
+
+        // Draw stacked bars
+        for (var j = 0; j < 8; j++) {
           var val = d[sectors[j]] || 0;
           var barHeight = p.map(val, 0, maxVal, 0, chartHeight);
-          p.fill(colors[j]);
+          p.fill(colors[j % colors.length]);
           p.noStroke();
           p.rect(x, yBottom - barHeight, barWidth, barHeight);
           yBottom -= barHeight;
@@ -171,8 +162,8 @@
       var lx = 900;
       var ly = 60;
       p.textAlign(p.LEFT);
-      for (var i = 0; i < sectors.length; i++) {
-        p.fill(colors[i]);
+      for (var i = 0; i < 8; i++) {
+        p.fill(colors[i % colors.length]);
         p.rect(lx, ly, 15, 15);
         p.fill(0);
         p.text(sectors[i], lx + 20, ly + 12);
